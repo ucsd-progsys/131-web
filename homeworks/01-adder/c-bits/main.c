e0379d37c1d5932cf71f082461833f07d57a35f8
Ranjit Jhala <jhala@cs.ucsd.edu>
2016-09-26 11:18:39 -0700

    update page

--- homeworks/01-adder/c-bits/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-extern int our_code_starts_here() asm("our_code_starts_here");
-
-int main(int argc, char** argv) {
-  printf("%d\n", our_code_starts_here());
-  return 0;
-}
--- homeworks/01-adder/tests/input/README.md
+++
@@ -1,1 +0,0 @@
-This directory is for creating test files that you want to write by hand.
